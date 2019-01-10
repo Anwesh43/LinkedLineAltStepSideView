3550616f988b6a0f83328d589b6f7eef3f8f1997
Anwesh43 <anweshthecool0@gmail.com>
2019-01-10 09:03:26 +0530

    adding LASNode class

--- linealtsidestepvew/src/main/java/com/anwesh/uiprojects/linealtsidestepvew/LineAltStepSideView.kt
+++ linealtsidestepvew/src/main/java/com/anwesh/uiprojects/linealtsidestepvew/LineAltStepSideView.kt
@@ -119,4 +119,48 @@ class LineAltStepSideView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LASNode(var i : Int, val state : State = State()) {
+
+        private var next : LASNode? = null
+        private var prev : LASNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LASNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLASNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LASNode {
+            var curr : LASNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
